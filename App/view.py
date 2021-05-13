912b579791c2244961073c7b484db9ec2e651d98
valentinac2311 <v.calderonm@uniandes.edu.co>
2021-05-13 10:41:32 -0500

    cambios

--- App/view.py
+++ App/view.py
@@ -44,7 +44,7 @@ operación seleccionada.
 # ___________________________________________________
 
 
-servicefile = 'bus_routes_14000.csv'
+servicefile = 'bus_routes_10000.csv'
 initialStation = None
 
 # ___________________________________________________
